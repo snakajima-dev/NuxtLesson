972e40a3b84ddc89717b8562784b2d47237700e1
teiru-web <snakajimadevelop@gmail.com>
2021-08-26 18:33:52 +0900

    first-commit

---
+++ config/message.js
@@ -0,0 +1,11 @@
+export const Message = {
+
+  EMPTY: "",
+  ERROR: {
+    EXISTS_USERNAME: "すでに使用されているユーザー名です",
+    USERNAME_LONG_ENOUGH: "ユーザー名は1文字以上30文字以内で入力してください",
+    PASSWORD_NOT_EQUALS: "パスワードと確認パスワードが一致しません",
+    PASSWORD_LONG_ENOUGH: "パスワードは6文字以上30文字以内で入力してください"
+  }
+
+}
